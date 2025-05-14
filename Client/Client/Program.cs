482338ad5fc66015953a6dd91b24cf47901eb115
biru109 <hbnhu1009@gmail.com>
2025-05-14 22:56:20 +0700

    Make connect btw menu and create

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Client
 {
-    class Program
+    static class Program
     {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // <-- Form khởi động của bạn
+        }
     }
 }
