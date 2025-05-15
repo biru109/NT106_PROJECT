116c3be6d84053a7efa7bbe055a516b60fd33200
nununu2610 <nnhu7732@gmail.com>
2025-05-15 14:01:39 +0700

    Add Client

---
+++ Client2/ConnectMenu.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace CLIENT
+{
+    partial class ConnectMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConnectMenu));
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.btnRules = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(220, 108);
+            this.textBoxName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(279, 22);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.Location = new System.Drawing.Point(235, 201);
+            this.textBoxIP.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(279, 22);
+            this.textBoxIP.TabIndex = 1;
+            this.textBoxIP.Text = "127.0.0.1";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(316, 244);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(115, 50);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "CREATE123";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Location = new System.Drawing.Point(316, 298);
+            this.btnJoin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(115, 50);
+            this.btnJoin.TabIndex = 3;
+            this.btnJoin.Text = "JOIN";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
+            // 
+            // btnRules
+            // 
+            this.btnRules.Location = new System.Drawing.Point(335, 391);
+            this.btnRules.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRules.Name = "btnRules";
+            this.btnRules.Size = new System.Drawing.Size(115, 50);
+            this.btnRules.TabIndex = 4;
+            this.btnRules.Text = "How 2 Play";
+            this.btnRules.UseVisualStyleBackColor = true;
+            this.btnRules.Click += new System.EventHandler(this.btnRules_Click);
+            // 
+            // ConnectMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(794, 452);
+            this.Controls.Add(this.btnRules);
+            this.Controls.Add(this.btnJoin);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.textBoxName);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ConnectMenu";
+            this.Text = "Welcome to UNO Game !";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Button btnRules;
+    }
+}
+
