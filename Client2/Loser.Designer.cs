53306341d458f178a4766d7647ce26c131372133
biru109 <hbnhu1009@gmail.com>
2025-05-15 15:01:59 +0700

    fix Client code

--- Client2/Loser.Designer.cs
+++ Client2/Loser.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace UNOClient
 {
-    partial class Form2_Lose
+    partial class Loser
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2_Lose));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loser));
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
