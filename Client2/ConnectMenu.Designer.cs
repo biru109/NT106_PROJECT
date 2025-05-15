d234192337b70cbb461038a87f0a17acaa15f5f5
biru109 <hbnhu1009@gmail.com>
2025-05-15 15:55:32 +0700

    update waiting room

--- Client2/ConnectMenu.Designer.cs
+++ Client2/ConnectMenu.Designer.cs
@@ -40,30 +40,30 @@ namespace CLIENT
             // textBoxName
             // 
             this.textBoxName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textBoxName.ForeColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBoxName.Location = new System.Drawing.Point(332, 174);
+            this.textBoxName.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.textBoxName.Location = new System.Drawing.Point(471, 138);
             this.textBoxName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxName.Multiline = true;
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(279, 27);
+            this.textBoxName.Size = new System.Drawing.Size(252, 29);
             this.textBoxName.TabIndex = 0;
-            this.textBoxName.Text = "Type in player\'s name";
             this.textBoxName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.textBoxName.MouseClick += new System.Windows.Forms.MouseEventHandler(this.textBoxName_MouseClick);
             // 
             // textBoxIP
             // 
+            this.textBoxIP.BackColor = System.Drawing.Color.White;
             this.textBoxIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.textBoxIP.Location = new System.Drawing.Point(332, 213);
+            this.textBoxIP.Location = new System.Drawing.Point(471, 202);
             this.textBoxIP.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxIP.Name = "textBoxIP";
-            this.textBoxIP.Size = new System.Drawing.Size(279, 27);
+            this.textBoxIP.Size = new System.Drawing.Size(252, 27);
             this.textBoxIP.TabIndex = 1;
             this.textBoxIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // btnCreate
             // 
             this.btnCreate.BackColor = System.Drawing.Color.Transparent;
-            this.btnCreate.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCreate.BackgroundImage")));
             this.btnCreate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnCreate.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCreate.FlatAppearance.BorderSize = 0;
@@ -71,10 +71,10 @@ namespace CLIENT
             this.btnCreate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.btnCreate.Location = new System.Drawing.Point(381, 244);
+            this.btnCreate.Location = new System.Drawing.Point(316, 254);
             this.btnCreate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(173, 60);
+            this.btnCreate.Size = new System.Drawing.Size(315, 43);
             this.btnCreate.TabIndex = 2;
             this.btnCreate.UseVisualStyleBackColor = false;
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
@@ -82,17 +82,16 @@ namespace CLIENT
             // btnJoin
             // 
             this.btnJoin.BackColor = System.Drawing.Color.Transparent;
-            this.btnJoin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnJoin.BackgroundImage")));
             this.btnJoin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnJoin.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnJoin.FlatAppearance.BorderSize = 0;
             this.btnJoin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.btnJoin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.btnJoin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnJoin.Location = new System.Drawing.Point(381, 308);
+            this.btnJoin.Location = new System.Drawing.Point(316, 322);
             this.btnJoin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnJoin.Name = "btnJoin";
-            this.btnJoin.Size = new System.Drawing.Size(190, 65);
+            this.btnJoin.Size = new System.Drawing.Size(328, 39);
             this.btnJoin.TabIndex = 3;
             this.btnJoin.UseVisualStyleBackColor = false;
             this.btnJoin.Click += new System.EventHandler(this.btnJoin_Click);
@@ -100,17 +99,16 @@ namespace CLIENT
             // btnRules
             // 
             this.btnRules.BackColor = System.Drawing.Color.Transparent;
-            this.btnRules.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRules.BackgroundImage")));
             this.btnRules.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btnRules.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnRules.FlatAppearance.BorderSize = 0;
             this.btnRules.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
             this.btnRules.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Orange;
             this.btnRules.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRules.Location = new System.Drawing.Point(383, 377);
+            this.btnRules.Location = new System.Drawing.Point(316, 380);
             this.btnRules.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnRules.Name = "btnRules";
-            this.btnRules.Size = new System.Drawing.Size(171, 57);
+            this.btnRules.Size = new System.Drawing.Size(326, 56);
             this.btnRules.TabIndex = 4;
             this.btnRules.UseVisualStyleBackColor = false;
             this.btnRules.Click += new System.EventHandler(this.btnRules_Click);
--- Client2/WaitingRoom.Designer.cs
+++ Client2/WaitingRoom.Designer.cs
@@ -32,17 +32,17 @@ namespace CLIENT
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WAITINGROOM));
             this.btnStart = new System.Windows.Forms.Button();
             this.groupBoxP1 = new System.Windows.Forms.GroupBox();
-            this.pictureBoxP1 = new System.Windows.Forms.PictureBox();
             this.labelP1 = new System.Windows.Forms.Label();
+            this.pictureBoxP1 = new System.Windows.Forms.PictureBox();
             this.groupBoxP2 = new System.Windows.Forms.GroupBox();
             this.labelP2 = new System.Windows.Forms.Label();
             this.pictureBoxP2 = new System.Windows.Forms.PictureBox();
             this.groupBoxP4 = new System.Windows.Forms.GroupBox();
-            this.labelP4 = new System.Windows.Forms.Label();
             this.pictureBoxP4 = new System.Windows.Forms.PictureBox();
+            this.labelP3 = new System.Windows.Forms.Label();
+            this.labelP4 = new System.Windows.Forms.Label();
             this.groupBoxP3 = new System.Windows.Forms.GroupBox();
             this.pictureBoxP3 = new System.Windows.Forms.PictureBox();
-            this.labelP3 = new System.Windows.Forms.Label();
             this.btnLeave = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.groupBoxP1.SuspendLayout();
@@ -57,65 +57,66 @@ namespace CLIENT
             // 
             // btnStart
             // 
-            this.btnStart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnStart.BackgroundImage")));
+            this.btnStart.BackColor = System.Drawing.Color.Transparent;
             this.btnStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnStart.FlatAppearance.BorderSize = 0;
             this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnStart.Location = new System.Drawing.Point(275, 422);
+            this.btnStart.Location = new System.Drawing.Point(162, 438);
             this.btnStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(135, 76);
+            this.btnStart.Size = new System.Drawing.Size(246, 42);
             this.btnStart.TabIndex = 0;
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.UseVisualStyleBackColor = false;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // groupBoxP1
             // 
-            this.groupBoxP1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBoxP1.BackgroundImage")));
-            this.groupBoxP1.Controls.Add(this.pictureBoxP1);
+            this.groupBoxP1.BackColor = System.Drawing.Color.Transparent;
             this.groupBoxP1.Controls.Add(this.labelP1);
-            this.groupBoxP1.Location = new System.Drawing.Point(12, 37);
+            this.groupBoxP1.Controls.Add(this.pictureBoxP1);
+            this.groupBoxP1.Location = new System.Drawing.Point(41, 110);
             this.groupBoxP1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBoxP1.Name = "groupBoxP1";
             this.groupBoxP1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBoxP1.Size = new System.Drawing.Size(216, 366);
+            this.groupBoxP1.Size = new System.Drawing.Size(199, 290);
             this.groupBoxP1.TabIndex = 1;
             this.groupBoxP1.TabStop = false;
             this.groupBoxP1.Text = "PLAYER 1";
-            // 
-            // pictureBoxP1
-            // 
-            this.pictureBoxP1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxP1.BackgroundImage")));
-            this.pictureBoxP1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBoxP1.Location = new System.Drawing.Point(13, 59);
-            this.pictureBoxP1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBoxP1.Name = "pictureBoxP1";
-            this.pictureBoxP1.Size = new System.Drawing.Size(189, 265);
-            this.pictureBoxP1.TabIndex = 1;
-            this.pictureBoxP1.TabStop = false;
             // 
             // labelP1
             // 
             this.labelP1.AutoSize = true;
             this.labelP1.BackColor = System.Drawing.Color.Transparent;
             this.labelP1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelP1.Location = new System.Drawing.Point(100, 326);
+            this.labelP1.Location = new System.Drawing.Point(120, 302);
             this.labelP1.Name = "labelP1";
             this.labelP1.Size = new System.Drawing.Size(25, 22);
             this.labelP1.TabIndex = 0;
             this.labelP1.Text = "...";
+            this.labelP1.Click += new System.EventHandler(this.labelP1_Click);
+            // 
+            // pictureBoxP1
+            // 
+            this.pictureBoxP1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxP1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxP1.Location = new System.Drawing.Point(6, 17);
+            this.pictureBoxP1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBoxP1.Name = "pictureBoxP1";
+            this.pictureBoxP1.Size = new System.Drawing.Size(184, 181);
+            this.pictureBoxP1.TabIndex = 1;
+            this.pictureBoxP1.TabStop = false;
             // 
             // groupBoxP2
             // 
-            this.groupBoxP2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBoxP2.BackgroundImage")));
+            this.groupBoxP2.BackColor = System.Drawing.Color.Transparent;
             this.groupBoxP2.Controls.Add(this.labelP2);
             this.groupBoxP2.Controls.Add(this.pictureBoxP2);
-            this.groupBoxP2.Location = new System.Drawing.Point(235, 37);
+            this.groupBoxP2.Location = new System.Drawing.Point(257, 127);
             this.groupBoxP2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBoxP2.Name = "groupBoxP2";
             this.groupBoxP2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBoxP2.Size = new System.Drawing.Size(216, 363);
+            this.groupBoxP2.Size = new System.Drawing.Size(193, 273);
             this.groupBoxP2.TabIndex = 2;
             this.groupBoxP2.TabStop = false;
             this.groupBoxP2.Text = "PLAYER 2";
@@ -125,7 +126,7 @@ namespace CLIENT
             this.labelP2.AutoSize = true;
             this.labelP2.BackColor = System.Drawing.Color.Transparent;
             this.labelP2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelP2.Location = new System.Drawing.Point(99, 326);
+            this.labelP2.Location = new System.Drawing.Point(78, 212);
             this.labelP2.Name = "labelP2";
             this.labelP2.Size = new System.Drawing.Size(25, 22);
             this.labelP2.TabIndex = 3;
@@ -133,100 +134,99 @@ namespace CLIENT
             // 
             // pictureBoxP2
             // 
-            this.pictureBoxP2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxP2.BackgroundImage")));
+            this.pictureBoxP2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBoxP2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBoxP2.Location = new System.Drawing.Point(13, 59);
+            this.pictureBoxP2.Location = new System.Drawing.Point(19, 19);
             this.pictureBoxP2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBoxP2.Name = "pictureBoxP2";
-            this.pictureBoxP2.Size = new System.Drawing.Size(189, 265);
+            this.pictureBoxP2.Size = new System.Drawing.Size(151, 162);
             this.pictureBoxP2.TabIndex = 2;
             this.pictureBoxP2.TabStop = false;
             // 
             // groupBoxP4
             // 
-            this.groupBoxP4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBoxP4.BackgroundImage")));
-            this.groupBoxP4.Controls.Add(this.labelP4);
+            this.groupBoxP4.BackColor = System.Drawing.Color.Transparent;
             this.groupBoxP4.Controls.Add(this.pictureBoxP4);
-            this.groupBoxP4.Location = new System.Drawing.Point(677, 37);
+            this.groupBoxP4.Controls.Add(this.labelP3);
+            this.groupBoxP4.Location = new System.Drawing.Point(681, 127);
             this.groupBoxP4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBoxP4.Name = "groupBoxP4";
             this.groupBoxP4.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBoxP4.Size = new System.Drawing.Size(216, 363);
+            this.groupBoxP4.Size = new System.Drawing.Size(184, 273);
             this.groupBoxP4.TabIndex = 3;
             this.groupBoxP4.TabStop = false;
             this.groupBoxP4.Text = "PLAYER 4";
             // 
-            // labelP4
-            // 
-            this.labelP4.AutoSize = true;
-            this.labelP4.BackColor = System.Drawing.Color.Transparent;
-            this.labelP4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelP4.Location = new System.Drawing.Point(96, 326);
-            this.labelP4.Name = "labelP4";
-            this.labelP4.Size = new System.Drawing.Size(25, 22);
-            this.labelP4.TabIndex = 1;
-            this.labelP4.Text = "...";
-            // 
             // pictureBoxP4
             // 
-            this.pictureBoxP4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxP4.BackgroundImage")));
+            this.pictureBoxP4.BackColor = System.Drawing.Color.Transparent;
             this.pictureBoxP4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBoxP4.Location = new System.Drawing.Point(13, 59);
+            this.pictureBoxP4.Location = new System.Drawing.Point(16, 19);
             this.pictureBoxP4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBoxP4.Name = "pictureBoxP4";
-            this.pictureBoxP4.Size = new System.Drawing.Size(189, 265);
+            this.pictureBoxP4.Size = new System.Drawing.Size(153, 154);
             this.pictureBoxP4.TabIndex = 2;
             this.pictureBoxP4.TabStop = false;
-            // 
-            // groupBoxP3
-            // 
-            this.groupBoxP3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("groupBoxP3.BackgroundImage")));
-            this.groupBoxP3.Controls.Add(this.pictureBoxP3);
-            this.groupBoxP3.Controls.Add(this.labelP3);
-            this.groupBoxP3.Location = new System.Drawing.Point(456, 37);
-            this.groupBoxP3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBoxP3.Name = "groupBoxP3";
-            this.groupBoxP3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBoxP3.Size = new System.Drawing.Size(216, 363);
-            this.groupBoxP3.TabIndex = 2;
-            this.groupBoxP3.TabStop = false;
-            this.groupBoxP3.Text = "PLAYER 3";
-            // 
-            // pictureBoxP3
-            // 
-            this.pictureBoxP3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxP3.BackgroundImage")));
-            this.pictureBoxP3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBoxP3.Location = new System.Drawing.Point(13, 59);
-            this.pictureBoxP3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pictureBoxP3.Name = "pictureBoxP3";
-            this.pictureBoxP3.Size = new System.Drawing.Size(189, 265);
-            this.pictureBoxP3.TabIndex = 2;
-            this.pictureBoxP3.TabStop = false;
             // 
             // labelP3
             // 
             this.labelP3.AutoSize = true;
             this.labelP3.BackColor = System.Drawing.Color.Transparent;
             this.labelP3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelP3.Location = new System.Drawing.Point(99, 326);
+            this.labelP3.Location = new System.Drawing.Point(78, 212);
             this.labelP3.Name = "labelP3";
             this.labelP3.Size = new System.Drawing.Size(25, 22);
             this.labelP3.TabIndex = 1;
             this.labelP3.Text = "...";
             // 
+            // labelP4
+            // 
+            this.labelP4.AutoSize = true;
+            this.labelP4.BackColor = System.Drawing.Color.Transparent;
+            this.labelP4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelP4.Location = new System.Drawing.Point(81, 229);
+            this.labelP4.Name = "labelP4";
+            this.labelP4.Size = new System.Drawing.Size(25, 22);
+            this.labelP4.TabIndex = 1;
+            this.labelP4.Text = "...";
+            // 
+            // groupBoxP3
+            // 
+            this.groupBoxP3.BackColor = System.Drawing.Color.Transparent;
+            this.groupBoxP3.Controls.Add(this.labelP4);
+            this.groupBoxP3.Controls.Add(this.pictureBoxP3);
+            this.groupBoxP3.Location = new System.Drawing.Point(472, 110);
+            this.groupBoxP3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBoxP3.Name = "groupBoxP3";
+            this.groupBoxP3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBoxP3.Size = new System.Drawing.Size(187, 290);
+            this.groupBoxP3.TabIndex = 2;
+            this.groupBoxP3.TabStop = false;
+            this.groupBoxP3.Text = "PLAYER 3";
+            // 
+            // pictureBoxP3
+            // 
+            this.pictureBoxP3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBoxP3.Location = new System.Drawing.Point(9, 17);
+            this.pictureBoxP3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBoxP3.Name = "pictureBoxP3";
+            this.pictureBoxP3.Size = new System.Drawing.Size(159, 184);
+            this.pictureBoxP3.TabIndex = 2;
+            this.pictureBoxP3.TabStop = false;
+            // 
             // btnLeave
             // 
-            this.btnLeave.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnLeave.BackgroundImage")));
+            this.btnLeave.BackColor = System.Drawing.Color.Transparent;
             this.btnLeave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.btnLeave.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnLeave.FlatAppearance.BorderSize = 0;
             this.btnLeave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLeave.Location = new System.Drawing.Point(505, 422);
+            this.btnLeave.Location = new System.Drawing.Point(501, 438);
             this.btnLeave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLeave.Name = "btnLeave";
-            this.btnLeave.Size = new System.Drawing.Size(135, 76);
+            this.btnLeave.Size = new System.Drawing.Size(243, 39);
             this.btnLeave.TabIndex = 4;
-            this.btnLeave.UseVisualStyleBackColor = true;
+            this.btnLeave.UseVisualStyleBackColor = false;
             this.btnLeave.Click += new System.EventHandler(this.btnLeave_Click);
             // 
             // richTextBox1
@@ -239,7 +239,7 @@ namespace CLIENT
             this.richTextBox1.Text = "";
             this.richTextBox1.Visible = false;
             // 
-            // WaitingRoom
+            // WAITINGROOM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -253,9 +253,10 @@ namespace CLIENT
             this.Controls.Add(this.groupBoxP3);
             this.Controls.Add(this.groupBoxP2);
             this.Controls.Add(this.groupBoxP1);
+            this.DoubleBuffered = true;
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
-            this.Name = "WaitingRoom";
+            this.Name = "WAITINGROOM";
             this.Text = "Game Lobby - Pending start ...";
             this.groupBoxP1.ResumeLayout(false);
             this.groupBoxP1.PerformLayout();
@@ -277,7 +278,6 @@ namespace CLIENT
 
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.GroupBox groupBoxP1;
-        private System.Windows.Forms.PictureBox pictureBoxP1;
         private System.Windows.Forms.Label labelP1;
         private System.Windows.Forms.GroupBox groupBoxP2;
         private System.Windows.Forms.Label labelP2;
@@ -290,5 +290,6 @@ namespace CLIENT
         private System.Windows.Forms.Label labelP3;
         private System.Windows.Forms.Button btnLeave;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBoxP1;
     }
 }
